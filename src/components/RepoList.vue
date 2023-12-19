45628ae3e0f3518dbe9202e4729eafff9e73b26b
Soumyanjalee <soumyanjaleemahapatra@gmail.com>
2023-12-19 10:40:24 +0100

    improved code quality

---
+++ src/components/RepoList.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul>
+		<li v-for="(repo, index) in repos" :key="repo.id" class="user__repo">
+			<div class="user__repo__item">
+				<span v-if='index===0' class="user__repo__item__label">Repository</span>
+				<span class="user__repo__item__text">{{ repo.full_name }}</span>
+			</div>
+			<div class="user__repo__item">
+				<span v-if='index===0' class="user__repo__item__label">Description</span>
+				<span class="user__repo__item__text">{{ repo.description }}</span>
+			</div>
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	props: {
+		repos: {
+			type: Array,
+			default: () => [],
+		}
+	}
+}	
+
+</script>
--- src/components/UserRepos.vue
+++ src/components/UserRepos.vue
@@ -1,77 +1,87 @@
 <template>
-<div class="user">
-    <h1> List of Github repositories for a user: </h1>
-    <input 
-        v-model="userName" 
-        placeholder="Enter username" 
-        :class="this.errors.length === 0 ? 'user__input' : 'user__input user__input--error'"
-        autofocus="autofocus"
-    />
-    <div v-if="this.errors.length!==0">
-        <span v-for="(err,index) in this.errors" :key="err[index]" class="user__error">
-            {{err}}
-        </span>
-    </div>
-    
-    <button v-on:click="getUserRepos" class="user__btn">List user repos in Git</button>
-    <div v-if="repos.length!==0" class="user__info">
-        <img :src = owner.avatar_url class="user__info__avatar">  
-        <span class="user__info__text">{{owner.login}}</span>
-        <ul v-for="repo in repos"  :key="repo" class="user__info__list">
-            <li class="user__info__list__item">
-                {{ repo.full_name }}
-            </li>
-            <li class="user__info__list__item">
-                {{ repo.description }}
-            </li>
-        </ul>
-    </div>
-    
-</div>
+	<div class="user">
+		<h1>List of Github repositories for a user</h1>
+		<input
+			v-model="userName"
+			placeholder="Enter Github username"
+			:class="{ 'user__input': !errors.length, 'user__input--error': errors.length }"
+		/>
+		<div v-if="errors">
+			<div v-for="(err, index) in errors" :key="index" class="user__error">
+				{{ err }}
+			</div>
+		</div>
+
+		<button @click="getUserRepos" class="user__btn">List user repos in Git</button>
+		<div v-if="isLoading">Loading...</div>
+		<div v-if="repos">
+			<img :src="owner.avatar_url" class="user__avatar" />
+			<span class="user__name">{{ owner.login }}</span>
+			<RepoList :repos="repos" />
+			<!-- <ul>
+				<li v-for="(repo, index) in repos" :key="repo.id" class="user__repo">
+					<div class="user__repo__item">
+						<span v-if='index===0' class="user__repo__item__label">Repository</span>
+						<span class="user__repo__item__text">{{ repo.full_name }}</span>
+					</div>
+					<div class="user__repo__item">
+						<span v-if='index===0' class="user__repo__item__label">Description</span>
+						<span class="user__repo__item__text">{{ repo.description }}</span>
+					</div>
+					
+				</li>
+			</ul> -->
+		</div>
+	</div>
 </template>
 
 <script>
+import RepoList from './RepoList.vue';
 export default {
-    name: 'UserRepos',
-    data() {
-        return{
-            repos:[],
-            owner: {
-                avatar_url:'' ,
-                login:'' 
-            },
-            errors:[]      
-        }
-    },
-    methods: {
-        async getUserRepos(){
-            if (this.repos.length !==0){
-                this.repos=[]
-            }
-            if (this.errors.length !==0){
-                this.errors=[]
-            }
-            if(this.userName){
-                try {
-                    const res = await fetch('https://api.github.com/users/'+this.userName +'/repos?per_page=250')
-                    const data = await res.json()
-                    if(res.status !== 200) {
-                        this.errors.push(res.statusText || 'Please try with a different user name')
-                        return;
-                    } 
-                    this.owner = data[0].owner
-                    data.forEach(element => {
-                        this.repos.push(element)
-                    });
-                }
-                catch(err){
-                    this.errors.push(err)
-                }
-                
-            } else {
-               this.errors.push('Please enter a username to access the repositiories') 
-            }
-        }
-    }
-}
+	components: {
+		RepoList
+	},
+	data() {
+		return {
+			repos: [],
+			owner: {
+				avatar_url: '',
+				login: '',
+			},
+			errors: [],
+			userName: '',
+			isLoading: false,
+		};
+	},
+	methods: {
+		async getUserRepos() {
+			this.repos = [];
+			this.errors = [];
+			this.isLoading = true;
+
+			if (!this.userName) {
+				this.errors.push('Please enter a username to access the repositories');
+				this.isLoading = false;
+				return;
+			}
+
+			try {
+				const res = await fetch(`https://api.github.com/users/${this.userName}/repos?per_page=250`);
+				const data = await res.json();
+
+				if (res.status !== 200) {
+					this.errors.push(res.statusText || 'Please try with a different username');
+					return;
+				}
+
+				this.owner = data[0].owner;
+				this.repos = data;
+			} catch (err) {
+				this.errors.push(err);
+			} finally {
+				this.isLoading = false;
+			}
+		},
+	},
+};
 </script>
